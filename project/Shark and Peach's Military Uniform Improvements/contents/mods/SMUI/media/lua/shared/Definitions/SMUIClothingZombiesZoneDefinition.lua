d8f83ef07aa79478037184a816acb68fc33c302d
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2023-01-13 20:05:09 -0500

    Cleaned zone definition code and reduced overall level of armed zombies spawning

--- project/Shark and Peach's Military Uniform Improvements/contents/mods/SMUI/media/lua/shared/Definitions/SMUIClothingZombiesZoneDefinition.lua
+++ project/Shark and Peach's Military Uniform Improvements/contents/mods/SMUI/media/lua/shared/Definitions/SMUIClothingZombiesZoneDefinition.lua
@@ -3,38 +3,17 @@ SMUIZombiesZoneDefinition = ZombiesZoneDefinition or {};
 
 SMUIZombiesZoneDefinition.Army = {
 
-	ArmyInstructorM = {
-		name="ArmyInstructor",
-		toSpawn=0,
-		mandatory="false",
-		gender="male",
-	},
-	SharkArmyInstructorM = {
-		name="SharkArmyInstructor",
-		toSpawn=0,
-		mandatory="false",
-		gender="male",
-	},
 	GhillieM = {
 		name="Ghillie",
 		toSpawn=0,
 		mandatory="false",
 		gender="male",
 	},		
-	ArmyInstructor = {
-		name="ArmyInstructor",
-		chance=0,
-		gender="male",
-	},
-	SharkArmyInstructor = {
-		name="SharkArmyInstructor",
-		chance=50,
+	SharkArmyCamoGreen = {
+		name="SharkArmyCamoGreen",
+		chance=70,
 		gender="male",
 	},	
-	ArmyCamoDesert = {
-		name="ArmyCamoDesert",
-		chance=0,
-	},
 	SharkArmyCamoDesertMale = {
 		name="SharkArmyCamoDesertMale",
 		chance=1,
@@ -50,18 +29,14 @@ SMUIZombiesZoneDefinition.Army = {
 		chance=1,
 		gender="female",
 	},	
-	ArmyCamoGreen = {
-		name="ArmyCamoGreen",
-		chance=0,
-	},
 	SharkArmyCamoGreenMale = {
 		name="SharkArmyCamoGreenMale",
-		chance=22,
+		chance=15,
 		gender="male",		
 	},
 	SharkArmyCamoGreenFemale = {
 		name="SharkArmyCamoGreenFemale",
-		chance=22,
+		chance=5,
 		gender="female",			
 	},
 	SharkArmyCamoGreenNVGMale = {
@@ -102,7 +77,6 @@ SMUIZombiesZoneDefinition.Army = {
 	SharkMilitaryPolice = {
 		name="SharkArmyCamoUrbanFemale",
 		chance=10,
-		gender="female",		
 	},
 	
 	
@@ -114,21 +88,6 @@ SMUIZombiesZoneDefinition.SecretBase = {
 		chance=1,
 		gender="male",
 	},
-	Priest = {
-		name="Priest",
-		toSpawn=1,
-		gender="male",
-		mandatory="true",
-	},
-	ArmyInstructor = {
-		name="ArmyInstructor",
-		chance=0,
-		gender="male",
-	},
-	ArmyCamoGreen = {
-		name="ArmyCamoGreen",
-		chance=0,
-	},
 	OfficeWorkerSkirt = {
 		name="OfficeWorkerSkirt",
 		chance=0,
@@ -139,17 +98,6 @@ SMUIZombiesZoneDefinition.SecretBase = {
 		chance=0,
 		gender="male",
 		beardStyles="null:80",
-	},
-	SharkArmyCamoUrbanGearedMale = {
-		name="SharkArmyCamoUrbanGearedMale",
-		gender="male",			
-		chance=0,
-	},
-	SharkCleanUpCrew = {
-		name="SharkArmyCamoUrbanMale",
-		gender="male",			
-		toSpawn=3,
-		chance=30,
 	},
 	SharkCleanUpCrew = {
 		name="SharkArmyCamoUrbanMale",
@@ -212,7 +160,7 @@ SMUIZombiesZoneDefinition.Default = ZombiesZoneDefinition.Default or {};
 table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkArmyCamoGreenMale", chance=0.01});
 table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkArmyCamoGreenFemale", chance=0.01});
 table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkArmyCamoGreenReservist", chance=0.01});
-table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkArmyInstructor", chance=0.01});
+table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkArmyCamoGreen", chance=0.04});
 table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkTankCrewman", chance=0.01});
 table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkMilitaryPolice", chance=0.01});
 table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkVeteran", chance=0.01});
